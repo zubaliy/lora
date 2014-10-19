82df426801057e5ba2600efd5691754a6e3fe1f1
Andriy Zubaliy <Andriy.Zubaliy@ordina.be>
2014-10-19 18:49:31 +0200

    clean

--- app/src/main/java/com/athome/zubaliy/nfctoolz/MainActivity.java
+++ app/src/main/java/com/athome/zubaliy/nfctoolz/MainActivity.java
@@ -37,22 +37,22 @@ public class MainActivity extends Activity {
         pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
                 getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
-        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED); try {
-            ndef.addDataType("*/*"); ndef.addCategory("DEFAULT");
-        } catch (MalformedMimeTypeException e) {
-            throw new RuntimeException("fail", e);
-        }
+    IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED); try {
+        ndef.addDataType("*/*"); ndef.addCategory("DEFAULT");
+    } catch (MalformedMimeTypeException e) {
+        throw new RuntimeException("fail", e);
+    }
 
-        IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+    IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
 
 
-        intentFiltersArray = new IntentFilter[]{ndef, tech};
+    intentFiltersArray = new IntentFilter[]{ndef, tech};
 
-        String[] techLists1 = new String[]{Ndef.class.getName(), NfcA.class.getName(),
-                MifareUltralight.class.getName()};
-        String[] techLists2 = new String[]{NdefFormatable.class.getName(), NfcA.class.getName(),
-                MifareUltralight.class.getName()};
-        techList = new String[][]{techLists1, techLists2};
+    String[] techLists1 = new String[]{Ndef.class.getName(), NfcA.class.getName(),
+            MifareUltralight.class.getName()};
+    String[] techLists2 = new String[]{NdefFormatable.class.getName(), NfcA.class.getName(),
+            MifareUltralight.class.getName()};
+    techList = new String[][]{techLists1, techLists2};
 
     }
 
@@ -75,34 +75,6 @@ public class MainActivity extends Activity {
 
         Toast.makeText(this, "New intent detected", Toast.LENGTH_SHORT).show();
 
-//        String action = intent.getAction();
-//        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-//
-//        String s = action + "\n\n" + tag.toString();
-//
-//        // parse through all NDEF messages and their records and pick ID type only
-//        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_ID);
-//        if (data != null) {
-//            try {
-//                for (int i = 0; i < data.length; i++) {
-//                    NdefRecord[] recs = ((NdefMessage) data[i]).getRecords();
-//                    for (int j = 0; j < recs.length; j++) {
-//                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
-//                                Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
-//                            byte[] payload = recs[j].getPayload();
-//                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
-//                            int langCodeLen = payload[0] & 0077;
-//
-//                            s += ("\n\nNdefMessage[" + i + "], NdefRecord[" + j + "]:\n\"" +
-//                                    new String(payload, langCodeLen + 1, payload.length - langCodeLen - 1,
-//                                            textEncoding) + "\"");
-//                        }
-//                    }
-//                }
-//            } catch (Exception e) {
-//                Log.e("TagDispatch", e.toString());
-//            }
-//        }
     }
 
     @Override
