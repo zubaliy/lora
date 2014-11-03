7f7c87f2cb82f7bdc3dfa3a027525c1a74fc8947
Andriy Zubaliy <Andriy.Zubaliy@ordina.be>
2014-11-03 22:25:56 +0100

    CONFIG: empty mac

--- app/src/main/java/com/athome/zubaliy/Util/Config.java
+++ app/src/main/java/com/athome/zubaliy/Util/Config.java
@@ -7,5 +7,5 @@ public class Config {
 
     public static final boolean DEBUG = true;
 
-    public static final String bluetoothMAC = "60:03:08:8C:5A:DB";
+    public static String bluetoothMAC = "";
 }
